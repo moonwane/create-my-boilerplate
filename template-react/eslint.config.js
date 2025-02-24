b50e0432781cbf605d98d329a6ee20db0046dfe1
Moonwane <moonwane@qq.com>
2025-02-24 18:36:46 +0800

    feat(template-react): update eslint config

--- template-react/eslint.config.js
+++ template-react/eslint.config.js
@@ -1,11 +1,16 @@
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import js from '@eslint/js';
 
 export default [
   ...tseslint.config(js.configs.recommended, tseslint.configs.recommended, {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
     },
@@ -14,19 +19,25 @@ export default [
   {
     files: ['apps/**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ];
